2bed2506a880c45dd3b45d8ddd8b52dcb33327d2
mwafrika <mwafrikajosue@gmail.com>
2019-04-20 21:18:59 +0200

    initial commit

--- javascript_files/admin.js
+++ javascript_files/admin.js
@@ -1,86 +1,105 @@
 
-(function ($) {
-    "use strict";
-    /*==================================================================
-    [ Focus input ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })
-    })
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+// (function ($) {
+//     "use strict";
+//     /*==================================================================
+//     [ Focus input ]*/
+//     $('.input100').each(function(){
+//         $(this).on('blur', function(){
+//             if($(this).val().trim() != "") {
+//                 $(this).addClass('has-val');
+//             }
+//             else {
+//                 $(this).removeClass('has-val');
+//             }
+//         })
+//     })
+//     /*==================================================================
+//     [ Validate ]*/
+//     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+//     $('.validate-form').on('submit',function(){
+//         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
+//         for(var i=0; i<input.length; i++) {
+//             if(validate(input[i]) == false){
+//                 showValidate(input[i]);
+//                 check=false;
+//             }
+//         }
 
-        return check;
-    });
+//         return check;
+//     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
+//     $('.validate-form .input100').each(function(){
+//         $(this).focus(function(){
+//            hideValidate(this);
+//         });
+//     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-   else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
+//     function validate (input) {
+//         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+//             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+//                 return false;
+//             }
+//         }
+//    else {
+//             if($(input).val().trim() == ''){
+//                 return false;
+//             }
+//         }
+//     }
+
+//     function showValidate(input) {
+//         var thisAlert = $(input).parent();
+
+//         $(thisAlert).addClass('alert-validate');
+//     }
+
+//     function hideValidate(input) {
+//         var thisAlert = $(input).parent();
+
+//         $(thisAlert).removeClass('alert-validate');
+//     }
+
+//     /*==================================================================
+//     [ Show pass ]*/
+//     var showPass = 0;
+//     $('.btn-show-pass').on('click', function(){
+//         if(showPass == 0) {
+//             $(this).next('input').attr('type','text');
+//             $(this).find('i').removeClass('fas fa-eye');
+//             $(this).find('i').addClass('fa-eye-slash');
+//             showPass = 1;
+//         }
+//         else {
+//             $(this).next('input').attr('type','password');
+//             $(this).find('i').addClass('fas fa-eye');
+//             $(this).find('i').removeClass('fa-eye-slash');
+//             showPass = 0;
+//         }
+
+//     });
+    
+// })(jQuery);
+
+// 
+document.querySelector('.open-nav').addEventListener('click', function(e){
+    var sidebar = document.querySelector('#mySidenav');
+    if(sidebar.getAttribute('data-state') === 'closed'){
+        sidebar.setAttribute('data-state','opened');
+        e.target.innerHTML = '&times;';
+        openNav();
     }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
+    else{
+        sidebar.setAttribute('data-state','closed');
+        e.target.innerHTML = '&#9776;';
+        closeNav();
     }
+});
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
 
-    /*==================================================================
-    [ Show pass ]*/
-    var showPass = 0;
-    $('.btn-show-pass').on('click', function(){
-        if(showPass == 0) {
-            $(this).next('input').attr('type','text');
-            $(this).find('i').removeClass('fas fa-eye');
-            $(this).find('i').addClass('fa-eye-slash');
-            showPass = 1;
-        }
-        else {
-            $(this).next('input').attr('type','password');
-            $(this).find('i').addClass('fas fa-eye');
-            $(this).find('i').removeClass('fa-eye-slash');
-            showPass = 0;
-        }
-
-    });
-})(jQuery);
 function openNav(){
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
