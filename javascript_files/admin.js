a81aadc4644d17f6d92ef668d7289d3b64697be7
mwafrika <mwafrikajosue@gmail.com>
2019-04-23 08:27:24 +0200

    initial commit

--- javascript_files/admin.js
+++ javascript_files/admin.js
@@ -1,92 +1,3 @@
-import '../css_files/loan_details.css';
-import '../Html_files/main.css';
-import '../css_files/admin.css';
-
-// (function ($) {
-//     "use strict";
-//     /*==================================================================
-//     [ Focus input ]*/
-//     $('.input100').each(function(){
-//         $(this).on('blur', function(){
-//             if($(this).val().trim() != "") {
-//                 $(this).addClass('has-val');
-//             }
-//             else {
-//                 $(this).removeClass('has-val');
-//             }
-//         })
-//     })
-//     /*==================================================================
-//     [ Validate ]*/
-//     var input = $('.validate-input .input100');
-
-//     $('.validate-form').on('submit',function(){
-//         var check = true;
-
-//         for(var i=0; i<input.length; i++) {
-//             if(validate(input[i]) == false){
-//                 showValidate(input[i]);
-//                 check=false;
-//             }
-//         }
-
-//         return check;
-//     });
-
-
-//     $('.validate-form .input100').each(function(){
-//         $(this).focus(function(){
-//            hideValidate(this);
-//         });
-//     });
-
-//     function validate (input) {
-//         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-//             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-//                 return false;
-//             }
-//         }
-//    else {
-//             if($(input).val().trim() == ''){
-//                 return false;
-//             }
-//         }
-//     }
-
-//     function showValidate(input) {
-//         var thisAlert = $(input).parent();
-
-//         $(thisAlert).addClass('alert-validate');
-//     }
-
-//     function hideValidate(input) {
-//         var thisAlert = $(input).parent();
-
-//         $(thisAlert).removeClass('alert-validate');
-//     }
-
-//     /*==================================================================
-//     [ Show pass ]*/
-//     var showPass = 0;
-//     $('.btn-show-pass').on('click', function(){
-//         if(showPass == 0) {
-//             $(this).next('input').attr('type','text');
-//             $(this).find('i').removeClass('fas fa-eye');
-//             $(this).find('i').addClass('fa-eye-slash');
-//             showPass = 1;
-//         }
-//         else {
-//             $(this).next('input').attr('type','password');
-//             $(this).find('i').addClass('fas fa-eye');
-//             $(this).find('i').removeClass('fa-eye-slash');
-//             showPass = 0;
-//         }
-
-//     });
-    
-// })(jQuery);
-
-// 
 document.querySelector('.open-nav').addEventListener('click',function(e){
     var sidebar = document.querySelector('#mySidenav');
     if(sidebar.getAttribute('data-state') === 'closed'){
@@ -104,7 +15,6 @@ function openNav(){
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "#fff";
-  // document.getElementById("mySidenav").style.marginTop ="90px";
   document.getElementById("pictures").style.width ="50px";
   document.getElementById("pictures").style.height ="50px";
 }
@@ -112,9 +22,4 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
-//   document.getElementById("mySidenav").style.marginTop ="250px";
-}
-
-function call_details(){
-  var detail = document.getElementById("myMain");
 }
