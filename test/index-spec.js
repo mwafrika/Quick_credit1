afab7252230b0de9d20cd6ca7971a81633a341da
mwafrika <mwafrikajosue@gmail.com>
2019-04-29 16:51:47 +0200

    add  configuration changes

---
+++ test/index-spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+const { expect } = require('chai');
+
+const server = require('../index');
+describe('test', () => {
+  it('the test should pass', () => {
+    expect('test').to.be.equal('test');
+  });
+});
