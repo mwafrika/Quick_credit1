eedc14f20ad0156c258e0e3315e8cf1b05be766e
mwafrika <mwafrikajosue@gmail.com>
2019-04-29 12:03:15 +0200

    modify path modules

---
+++ test/index-spec.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+const { expect } = require('chai');
+
+const server = require('../index');
+
+describe('test', () => {
+  it('the test should pass', () => {
+    expect('test').to.be.equal('test');
+  });
+});
