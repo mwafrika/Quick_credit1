2ab60c2320de5fe5f2864219270b3360b2c93db1
mwafrika <mwafrikajosue@gmail.com>
2019-05-02 10:15:14 +0200

    test the test

--- test/index-spec.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-undef */
-describe('test', () => {
-  it('the test should pass', () => {
-    expect('test').to.be.equal('test');
-  // eslint-disable-next-line linebreak-style
-  });
-});
-
