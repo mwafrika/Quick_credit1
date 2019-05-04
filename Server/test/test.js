1f887916efd00337624fbee568d97aa7dd94401d
mwafrika <mwafrikajosue@gmail.com>
2019-05-04 16:56:05 +0200

    bug(review files):arrange files[fix]

--- Server/test/test.js
+++ Server/test/test.js
@@ -1,14 +1,12 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable comma-spacing */
-/* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
-
+/* eslint-disable import/no-extraneous-dependencies */
 const assert = require('assert');
 
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
-      assert.equal([1,2,3].indexOf(4), -1);
+      assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });
