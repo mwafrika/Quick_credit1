57dcb82645e6dcf3df0570f0cdae68a7c8ea5c6d
mwafrika <mwafrikajosue@gmail.com>
2019-05-12 22:55:36 +0200

    link ui to heroku

--- Server/config/index.js
+++ Server/config/index.js
@@ -11,7 +11,7 @@ const app = express();
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/UI')));
+app.use(express.static(path.join(__dirname, '/UI/')));
 app.use('/', router);
 app.get('/test', (req, res) => {
   res.send('mwafrika');
